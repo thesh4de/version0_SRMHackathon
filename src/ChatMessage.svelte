70a6d5c8d16d77c3d6ff21273ac2be1a011c437b
thesh4de <tejas1002ashok@gmail.com>
2022-04-02 20:57:52 +0530

    Added previous files

---
+++ src/ChatMessage.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let message;
+  export let sender;
+
+  const messageClass = message.who === sender ? 'sent' : 'received';
+
+  const ts = new Date(message.when);
+</script>
+
+<div class={`message ${messageClass}`}>
+  <div class="message-text">
+    <p>{message.what}</p>
+
+    <time>{ts.toLocaleTimeString()}</time>
+  </div>
+</div>
---
+++ src/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { username, user } from './user';
+
+  function signout() {
+    user.leave();
+    username.set('');
+  }
+</script>
+
+<header>
+<h1>D-Chat</h1>
+  {#if $username}
+    <div class="user-bio">
+      
+      <span>Hello <strong>{$username}</strong></span>
+    </div>
+
+    <button class="signout-button" on:click={signout}>Sign Out</button>
+
+    {:else}
+
+      <h3>D-Chat</h3>
+  {/if}
+</header>
