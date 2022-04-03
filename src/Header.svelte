60a25dcc19f82f98e7400b8a25bbfa3c65876e1a
thesh4de <tejas1002ashok@gmail.com>
2022-04-03 09:40:08 +0530

    Updated UI

--- src/Header.svelte
+++ src/Header.svelte
@@ -1,6 +1,49 @@
+
+<style> 
+  .signout-button{
+      background-color: lightblue;
+      color: black;
+      border-radius: 15px;
+      border: 1px solid black;
+  }
+  .signout-button:hover{
+    color: white;
+    background-color: red;
+  }
+  div.logo{
+    color: white;
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    font-weight: 300;
+    font-size: x-large;
+  }
+  div.logo{
+    width: 30%;
+    background-color: white;
+    height: 45px;
+    color: blue;
+    text-align: center;
+    vertical-align: middle;
+    line-height: 45px ;
+    border-radius: 20px;
+    cursor: pointer;
+  }
+  .community{
+    font-weight: 300;
+    font-size: large;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    border: 1px solid black;
+    padding: 5px;
+    background-color: skyblue;
+    color: black;
+    border-radius: 10px;
+    
+  }
+  a{
+    text-decoration: none;
+  }
+</style>
 <script>
   import { username, user } from './user';
-
   function signout() {
     user.leave();
     username.set('');
@@ -8,17 +51,16 @@
 </script>
 
 <header>
-<h1>D-Chat</h1>
+<div class="logo">P2P Messenger</div>
   {#if $username}
     <div class="user-bio">
       
       <span>Hello <strong>{$username}</strong></span>
+      <img src={`https://avatars.dicebear.com/api/initials/${$username}.svg`} alt="avatar" /> 
     </div>
 
     <button class="signout-button" on:click={signout}>Sign Out</button>
-
     {:else}
-
-      <h3>D-Chat</h3>
-  {/if}
+      <h3 class="community"><a href="https://gun.eco/" target="_blank">Made for DApp community</a></h3>
+    {/if}
 </header>
